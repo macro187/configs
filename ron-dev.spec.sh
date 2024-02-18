809214036c132b214c9dd389ee54b9fb3d7a46e4
Ron MacNeil <macro@hotmail.com>
2024-02-18 22:41:47 +0900

    ron-dev: Work on container or machine

--- ron-dev.spec.sh
+++ ron-dev.spec.sh
@@ -5,6 +5,13 @@ include dev
 
 include _vim
 
-feature container-cook
 feature ron-vim-plugins
 feature ron-nvim-plugins
+
+if [ "$HOTBOX_TARGET" = "container" ] ; then
+    feature container-cook
+    feature container-hotbox
+else
+    feature cook
+    feature hotbox
+fi
